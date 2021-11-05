27a5a186181537bdf45bf1d973c67f90f543618c
Shingo Omura <everpeace@gmail.com>
2021-11-05 19:04:17 +0900

    fix tool versions in flexci

--- .flexci/run.sh
+++ .flexci/run.sh
@@ -2,12 +2,13 @@
 set -eux
 set -o pipefail
 
-curl -o /tmp/go.tar.gz https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
+rm -rf /usr/local/go
+curl -Lo /tmp/go.tar.gz https://golang.org/dl/go1.16.10.linux-amd64.tar.gz
 tar -C /usr/local -xzf /tmp/go.tar.gz
-curl -Lo /tmp/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64
+curl -Lo /tmp/kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x /tmp/kind
 mv /tmp/kind /usr/local/bin/kind
-curl -Lo /tmp/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/linux/amd64/kubectl
+curl -Lo /tmp/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.21.4/bin/linux/amd64/kubectl
 chmod +x /tmp/kubectl
 mv /tmp/kubectl /usr/local/bin/kubectl
 
